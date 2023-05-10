439ea22aa0aaca918a46335f3b6d537fdcb2f835
Joshua Akinola <akinolajsh@gmail.com>
2023-05-10 02:24:35 +0100

    second version

--- index.ts
+++ index.ts
@@ -1,23 +1,51 @@
-class Stack{
-    content: any[]
-    constructor(){
-            this.content = [];
-        }
+// class Stack{
+//     content: any[]
+//     constructor(){
+//             this.content = [];
+//         }
 
-        addtoStack(val){
-            let stack = this.content.push(val)
-            console.log(this.content)
-        }
+//         addtoStack(val){
+//             let stack = this.content.push(val)
+//             console.log(this.content)
+//         }
 
-        removefromStack(){
-            this.content.pop()
-            console.log(this.content)
-        }
+//         removefromStack(){
+//             this.content.pop()
+//             console.log(this.content)
+//         }
+// }
+
+// let stack = new Stack()
+
+// stack.addtoStack("Akin")
+// stack.addtoStack("Austine")
+// stack.addtoStack("Williams")
+// stack.removefromStack()
+
+class Queue {
+  content: any[];
+  constructor() {
+    this.content = [];
+  }
+
+  addtoQueue(val) {
+    let queue = this.content.push(val);
+    console.log(this.content);
+  }
+
+  removefromQueue() {
+    this.content.shift();
+    console.log(this.content);
+  }
 }
 
-let stack = new Stack()
+let queue = new Queue();
 
-stack.addtoStack("Akin")
-stack.addtoStack("Austine")
-stack.addtoStack("Williams")
-stack.removefromStack()
+queue.addtoQueue("Dog");
+
+queue.addtoQueue("Cow");
+
+queue.addtoQueue("Rat");
+
+queue.removefromQueue();
+queue.removefromQueue();
